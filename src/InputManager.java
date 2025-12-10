15a547dbcc96dae39efacd2d3589e9a81ba83dee
bedbosg <edoardo.ratti12@gmail.com>
2025-12-10 15:29:13 +0100

    CIao

--- src/InputManager.java
+++ src/InputManager.java
@@ -1,3 +1,26 @@
+import java.util.Scanner;
+
 public class InputManager{
+    public Scanner scanner;
+    InputManager(){
+        scanner = new Scanner(System.in);
+    }
     
+    public char getPlayerCharacter(){
+        System.out.println("Please insert a character who you wanna play in this game");
+        String input;
+        while(!scanner.hasNext() || (input = scanner.next()).length() != 1 || input == " "){
+            System.err.println("Cannot insert a word longer than a character");
+            System.out.println("Again: please insert a single character othr than space");
+            scanner.nextLine();
+        }
+        return input.charAt(0);
+    }
+    public String getPlayerName(){
+        return scanner.next();
+    }
+
+    public void release (){
+        scanner.close();
+    }
 }
