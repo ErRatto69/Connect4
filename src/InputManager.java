5270aa0b6cacce2a95563ea1f0bb07584a0ccbdc
bedbosg <edoardo.ratti12@gmail.com>
2025-12-11 08:44:09 +0100

    Add files via upload

--- src/InputManager.java
+++ src/InputManager.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class InputManager{
+    public static final String[] COLOR_LABELS = {"Red", "Green", "Yellow", "Blu", "Mangenta","Cyan","White"};
     public Scanner scanner;
     InputManager(){
         scanner = new Scanner(System.in);
@@ -18,6 +19,21 @@ public class InputManager{
     }
     public String getPlayerName(){
         return scanner.next();
+    }
+    public int getColor(){
+        int color;
+        while (!scanner.hasNext() || (color =  scanner.nextInt()) < 1 ||  color > 7 ) {
+            System.err.println("Insert a number relative one color");
+            System.out.println("Please insert a number between 1 and seven");
+            scanner.nextLine();
+        }
+        return color;
+    }
+    public void showColors(){
+        //Colors goes between 41 and 47, (change color \u001b[31m) (reset \u001b[0m) everytime reset
+        for (int i = 0; i < COLOR_LABELS.length; i++) {
+            System.out.println(COLOR_LABELS[i] + i);
+        }
     }
 
     public void release (){
